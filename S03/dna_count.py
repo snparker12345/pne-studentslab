aae446393c73ee9ff129c60a08fcacedd77050ea
snparker12345 <snparker12345@gmail.com>
2024-01-31 18:30:56 +0100

    done modified version

--- S03/dna_count.py
+++ S03/dna_count.py
@@ -1,12 +1,16 @@
-stuff = input("introduce the sequence: ")
-dnanums = {'A': 0, 'C':0, 'T':0, 'G':0}
-numchars = len(stuff)
-print(numchars)
-for i in stuff:
-    if i in dnanums:
-        val = dnanums.get(i) + 1
-        dnanums[i] = val
-print("A: ", dnanums.get("A"))
-print("C: ", dnanums.get("C"))
-print("T: ", dnanums.get("T"))
-print("G: ", dnanums.get("G"))
+filename = input("enter the filename: ")
+
+with (open(filename, "r") as stuff):
+    final_string = ""
+    final_string += stuff.readline()
+    dnanums = {'A': 0, 'C':0, 'T':0, 'G':0}
+    numchars = len(final_string)
+    print(numchars)
+    for i in final_string:
+        if i in dnanums:
+            val = dnanums.get(i) + 1
+            dnanums[i] = val
+    print("A: ", dnanums.get("A"))
+    print("C: ", dnanums.get("C"))
+    print("T: ", dnanums.get("T"))
+    print("G: ", dnanums.get("G"))
