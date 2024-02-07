2e69380f2e23fa03db355f7447ffe3bc82d164b8
snparker12345 <snparker12345@gmail.com>
2024-02-07 15:42:04 +0100

    done

--- S04/sequence.py
+++ S04/sequence.py
@@ -1,17 +1,19 @@
 from pathlib import Path
 
 # -- Constant with the new of the file to open
-FILENAME = "U5.txt"
+FILENAME = "ADA.txt"
 
 # -- Open and read the file
 file_contents = Path(FILENAME).read_text()
 
 file_contents = file_contents.split("\n")
+
 print("Body of the U5.txt file:")
-lineval = 0
+length = 0
+linenum = 0
 for line in file_contents:
-    if lineval != 0:
+    if linenum != 0:
         print(line)
-    lineval += 1
-
-
+        length += len(line)
+    linenum += 1
+print(length)
