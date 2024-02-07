d3f7a7903b53da7d38dea762b3de381287b0190f
snparker12345 <snparker12345@gmail.com>
2024-02-07 15:16:46 +0100

    almost done

---
+++ S04/header.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+# -- Constant with the new of the file to open
+FILENAME = "RNU6_269P.txt"
+
+# -- Open and read the file
+file_contents = Path(FILENAME).read_text()
+
+file_contents = file_contents.split("\n")
+
+file_contents = file_contents[0]
+# -- Print the contents on the console
+print("First line of the RNU6_269P.txt file:")
+print(file_contents)
