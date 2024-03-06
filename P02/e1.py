45e72419560702d2532f54491377663c9cadbd35
snparker12345 <snparker12345@gmail.com>
2024-03-06 18:21:17 +0100

    edited stuff

---
+++ P02/e1.py
@@ -0,0 +1,22 @@
+from Client0 import Client
+
+PRACTICE = 2
+EXERCISE = 1
+
+print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
+
+# -- Parameters of the server to talk to
+IP = "127.0.0.1"  # your IP address
+PORT = 8080
+
+# -- Create a client object
+c = Client(IP, PORT)
+
+# -- Test the ping method
+c.ping()
+
+print(c)
+
+print("Sending a message to the server...")
+response = c.talk("Testing!!!")
+print(f"Response: {response}")
