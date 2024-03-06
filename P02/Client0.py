fedc61cfa414e5612de3bfbb65a2946aa6e4d481
snparker12345 <snparker12345@gmail.com>
2024-03-06 18:22:27 +0100

    edited stuff

---
+++ P02/Client0.py
@@ -0,0 +1,35 @@
+import socket
+
+
+class Client:
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+
+    def ping(self):
+        print(f"OK!")
+
+    def __str__(self):
+        ip = self.ip
+        port = self.port
+        return f"Connection to SERVER at {ip}, PORT: {port}"
+
+    def talk(self, msg):
+        # -- Create the socket
+        port = self.port
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # establish the connection to the Server (IP, PORT)
+        s.connect((self.ip, self.port))
+
+        # Send data.
+        s.send(str.encode(msg))
+
+        # Receive data
+        response = s.recv(2048).decode("utf-8")
+
+        # Close the socket
+        s.close()
+
+        # Return the response
+        return response
