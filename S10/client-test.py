d5899dae2b060dc1817350f936849bd6b8a28735
snparker12345 <snparker12345@gmail.com>
2024-03-20 17:04:52 +0100

    done

--- S10/client-test.py
+++ S10/client-test.py
@@ -6,13 +6,13 @@ import socket
 # Write here the correct parameter for connecting to the
 # Teacher's server
 PORT = 8080
-IP = "212.128.254.253" # it depends on the machine the server is running
+IP = "212.128.255.67" # it depends on the machine the server is running
 client = Client(IP, PORT)
 
 try:
         # Send messages
     for i in range(5):
-        message = f"message {i}"
+        message = f"Test {i}..."
         client.talk(message)
         print(f"Sent: {message}")
 except Exception as e:
