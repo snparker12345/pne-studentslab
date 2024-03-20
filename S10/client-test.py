3249ee6d6545e89a4be5b51d1086e4071c938b17
snparker12345 <snparker12345@gmail.com>
2024-03-20 16:41:06 +0100

    almost done

--- S10/client-test.py
+++ S10/client-test.py
@@ -5,17 +5,9 @@ import socket
 # SERVER IP, PORT
 # Write here the correct parameter for connecting to the
 # Teacher's server
-PORT = 8081
-IP = "192.168.124.179" # it depends on the machine the server is running
+PORT = 8080
+IP = "212.128.254.253" # it depends on the machine the server is running
 client = Client(IP, PORT)
-
-# First, create the socket
-# We will always use these parameters: AF_INET y SOCK_STREAM
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# establish the connection to the Server (IP, PORT)
-s.connect((IP, PORT))
-
 
 try:
         # Send messages
@@ -25,8 +17,5 @@ try:
         print(f"Sent: {message}")
 except Exception as e:
     print("An error occurred:", e)
-finally:
-        # Close the connection
-    s.close()
 
 
