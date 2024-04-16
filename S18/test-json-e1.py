01eec89b918c54dd5ea9a617f5229351ff23129c
snparker12345 <snparker12345@gmail.com>
2024-04-16 15:55:17 +0200

    added all my files

---
+++ S18/test-json-e1.py
@@ -0,0 +1,38 @@
+import json
+import termcolor
+from pathlib import Path
+
+
+# -- Read the json file
+jsonstring = Path("people-e1.json").read_text()
+
+people = {}
+# Create the object person from the json string
+people = json.loads(jsonstring)
+
+# Person is now a dictionary. We can read the values
+# associated to the fields 'Firstname', 'Lastname' and 'age'
+for person in people:
+# Print the information on the console, in colors
+    print()
+    termcolor.cprint("Name: ", 'green', end="")
+    print(person['Firstname'], person['Lastname'])
+    termcolor.cprint("Age: ", 'green', end="")
+    print(person['age'])
+
+# Get the phoneNumber list
+    phoneNumbers = person['phoneNumber']
+
+# Print the number of elements in the list
+    termcolor.cprint("Phone numbers: ", 'green', end='')
+    print(len(phoneNumbers))
+
+# Print all the numbers
+    for i, dictnum in enumerate(phoneNumbers):
+        termcolor.cprint("  Phone " + str(i + 1) + ": ", 'blue')
+
+    # The element num contains 2 fields: number and type
+        termcolor.cprint("\t- Type: ", 'red', end='')
+        print(dictnum['type'])
+        termcolor.cprint("\t- Number: ", 'red', end='')
+        print(dictnum['number'])
