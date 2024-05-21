661f80426e130a448d7416bfcb7eaa5f241ca540
snparker12345 <snparker12345@gmail.com>
2024-05-21 17:37:18 +0200

    done

--- Final-project/server.py
+++ Final-project/server.py
@@ -52,6 +52,8 @@ def get_species():
     # -- Read the response's body
     response = json.loads(r1.read().decode("utf-8"))
 
+    if r1.status == 400:
+        return None
     species_data = response['species']
 
     species_names = [species["name"] for species in species_data]
@@ -60,6 +62,77 @@ def get_species():
     # print(species_names)
     return species_names
 
+def get_karyotype(specie):
+    SERVER = 'rest.ensembl.org'
+    ENDPOINT = '/info/assembly/'
+    PARAMS = '?content-type=application/json'
+    print(f"Server: {SERVER}")
+    request = ENDPOINT + specie + PARAMS
+    url = SERVER + ENDPOINT + specie + PARAMS
+
+    print()
+
+    # Connect with the server
+    conn = http.client.HTTPConnection(SERVER)
+    try:
+        conn.request("GET", request)
+    except ConnectionRefusedError:
+        print("ERROR! Cannot connect to the Server")
+        exit()
+
+        # -- Read the response message from the server
+    r1 = conn.getresponse()
+
+    print(f"Response received!: {r1.status} {r1.reason}\n")
+
+    if r1.status == 400:
+        return None
+
+    # -- Read the response's body
+    response = json.loads(r1.read().decode("utf-8"))
+
+    karyotype_data = response['karyotype']
+
+    # Print the list of species names
+    # print(species_names)
+    return karyotype_data
+
+def get_chromosome_length(specie, chrom):
+    SERVER = 'rest.ensembl.org'
+    ENDPOINT = '/info/assembly/'
+    PARAMS = '?content-type=application/json'
+    print(f"Server: {SERVER}")
+    request = ENDPOINT + specie + PARAMS
+    url = SERVER + ENDPOINT + specie + PARAMS
+
+    print()
+
+    # Connect with the server
+    conn = http.client.HTTPConnection(SERVER)
+    try:
+        conn.request("GET", request)
+    except ConnectionRefusedError:
+        print("ERROR! Cannot connect to the Server")
+        exit()
+
+        # -- Read the response message from the server
+    r1 = conn.getresponse()
+
+    print(f"Response received!: {r1.status} {r1.reason}\n")
+
+    if r1.status == 400:
+        return None
+    else:
+    # -- Read the response's body
+        response = json.loads(r1.read().decode("utf-8"))
+
+        chromosome_data = response['top_level_region']
+
+        for chromosome in chromosome_data:
+            if chromosome.get("name") == chrom:
+                return chromosome.get("length")
+            else:
+                return None
 
 class TestHandler(http.server.BaseHTTPRequestHandler):
 
@@ -78,24 +151,46 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         print("self path", self.path)
         if self.path.__contains__("listSpecies"):
             arguments = parse_qs(url_path.query)
-            limit = int(arguments.get("limit")[0])
+            limit = arguments.get("limit")[0]
             species = get_species()
+            if species == None:
+                contents = Path('html/error.html').read_text()
+            else:
+                num = len(species)
+                if limit != None:
+                    limit = int(limit)
+                    species = species[:limit]
 
-            num = len(species)
-            species = species[:limit]
-            species_list = ""
-            for specie in species:
-                species_list += f"<li>{specie}</li>"
-            contents = read_html_file("species.html").render(
-                context={"species": species_list, "num": num, "limit": limit})
+
+                species_list = ""
+                for specie in species:
+                    species_list += f"<li>{specie}</li>"
+                contents = read_html_file("species.html").render(
+                    context={"species": species_list, "num": num, "limit": limit})
                 # make each word have a * and then submit into the html
         elif self.path.__contains__("karyotype"):
-            contents = Path('html/ping.html').read_text()
+            arguments = parse_qs(url_path.query)
+            id = arguments.get("speciesK")[0]
+            karyotype_list = get_karyotype(id)
+            if karyotype_list == None:
+                contents = Path('html/error.html').read_text()
+            else:
+                karyotypes = ""
+                for specie in karyotype_list:
+                    karyotypes += f"<li>{specie}</li>"
+                contents = read_html_file("karyotype.html").render(
+                    context={"chromosomeList": karyotypes})
+
             # we get it from here
         elif self.path.__contains__("chromosomeLength"):
             arguments = parse_qs(url_path.query)
-          #  arg = arguments.get("operation")[0]
-          #  contents = read_html_file("get.html").render(context={"todisplay": text, "otherdisplay": num})
+            specie = arguments.get("speciesL")[0]
+            chrom = arguments.get("chromosome")[0]
+            chrom_len = get_chromosome_length(specie, chrom)
+            if (chrom_len == None):
+                contents = Path('html/error.html').read_text()
+            else:
+                contents = read_html_file("length.html").render(context={"chromosomeLength": chrom_len})
         elif self.path.__contains__("gene-button"):
             arguments = parse_qs(url_path.query)
           #  arg = arguments.get("gene")[0]
